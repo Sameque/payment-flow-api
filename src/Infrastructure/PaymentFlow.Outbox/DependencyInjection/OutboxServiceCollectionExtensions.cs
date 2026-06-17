ef18ed2bde9ac5bf65487fb3e902fba0d68f6dfc
Sameque Sant' ana <samequesantana@gmail.com>
2026-06-17 01:44:01 -0300

    refactor: separate outbox writer from dispatcher

--- src/Infrastructure/PaymentFlow.Outbox/DependencyInjection/OutboxServiceCollectionExtensions.cs
+++ src/Infrastructure/PaymentFlow.Outbox/DependencyInjection/OutboxServiceCollectionExtensions.cs
@@ -5,10 +5,17 @@ namespace PaymentFlow.Outbox;
 
 public static class OutboxServiceCollectionExtensions
 {
-    public static IServiceCollection AddPaymentFlowOutbox(this IServiceCollection services, IConfiguration configuration)
+    public static IServiceCollection AddPaymentFlowOutboxWriter(this IServiceCollection services, IConfiguration configuration)
     {
         services.Configure<OutboxDispatcherOptions>(configuration.GetSection(OutboxDispatcherOptions.SectionName));
         services.AddScoped<IOutboxMessageWriter, OutboxMessageWriter>();
+
+        return services;
+    }
+
+    public static IServiceCollection AddPaymentFlowOutboxDispatcher(this IServiceCollection services, IConfiguration configuration)
+    {
+        services.AddPaymentFlowOutboxWriter(configuration);
         services.AddHostedService<OutboxDispatcher>();
 
         return services;
